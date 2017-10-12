0e3ba65f8764411ad676178074487e8716752e84
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2017-10-12 19:11:47 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012220123) do
+ActiveRecord::Schema.define(version: 20171012221021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20171012220123) do
     t.float "valor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "lote_id"
+    t.index ["lote_id"], name: "index_produtos_on_lote_id"
   end
 
+  add_foreign_key "produtos", "lotes"
 end
